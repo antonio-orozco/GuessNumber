2423cdbcd1a5ced1d850d75446146dce37c196a5
Antonio Orozco <33075992+leads1570@users.noreply.github.com>
2019-07-18 21:06:46 -0500

    Add files via upload

---
+++ GuessNumber.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Week2
+{
+    class GuessNumber
+    {
+        static void Main(string[] args)
+        {
+            string response;
+            do
+            {
+                int secret = RandomNumber(1, 100);
+                do
+                {
+                    
+                    int guess = Utils.GetNumber("Enter number between 1 and 100: ", 100);
+                    int highOrLow = PlayGame(secret, guess);
+
+                    if (highOrLow == 0)
+                    {
+                        Console.WriteLine("You win!");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your guess is too " + (highOrLow == 1 ? "High" : "Low"));
+                    }
+
+                } while (true);
+                response = Utils.GetInput("Want a new game? ");
+            } while (! response.Equals("N"));
+        }
+        static int PlayGame(int secret, int guess)
+        {
+            return (secret == guess) ? 0 : (guess < secret) ? -1 : 1;
+        }
+
+        static int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+
+       
+    }
+}
